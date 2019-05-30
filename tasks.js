011322fa1df4b73fc77d42163d260ac74bf1fca1
simoncobb1966 <simoncobb1966@gmail.com>
2019-05-30 13:06:45 +0100

    Deleteing working OK - need to deploy

--- tasks.js
+++ tasks.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express")
 const serverless = require("serverless-http")
 
@@ -11,19 +9,13 @@ const connection=mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  // host: "todoapplication.cfwgd2eitm3t.eu-west-2.rds.amazonaws.com",
-  // user:"root",
-  // password:"bradford1",
   database:"todoapplication"
 })
 
 //connection.connect()
 
-
-
 app.get("/tasks", function (request, response) {
 // "/tasks" - is because the url has "/tasks" after the endpoint.
-
 
   connection.query("SELECT * FROM Tasks", function(err,result,fields) {
  if (err!==null) {
@@ -35,11 +27,20 @@ app.get("/tasks", function (request, response) {
   })
 })
 
-  // response.json({
-  //   // message: `Title ${title} requested the task. ${returnData}`
-  //   tasks: "test"
-  // })
 
+app.delete("/tasks/:id", function (request, response) {
+  const taskId=request.params.id
+  console.log(taskId)
+  connection.query("DELETE FROM Tasks WHERE TaskId = ?", [taskId], function(err, result, fields) {
+    if (err!==null) {
+      console.log("Something went wrong deleting the task", err)
+      response.send(500)
+    } else {
+    response.send ("Item Deleted")
+    }
+  })
+
+})
 
 
 module.exports.handler = serverless(app)
@@ -47,3 +48,6 @@ module.exports.handler = serverless(app)
 // https://f8nibhiadf.execute-api.eu-west-2.amazonaws.com/dev/tasks?username=simon
 // to deploy:- serverless deploy function --function tasks
 // if just the tasks file has been update you can use 'serverless deploy' on its own
+  // host: "todoapplication.cfwgd2eitm3t.eu-west-2.rds.amazonaws.com",
+  // user:"root",
+  // password:"bradford1",
